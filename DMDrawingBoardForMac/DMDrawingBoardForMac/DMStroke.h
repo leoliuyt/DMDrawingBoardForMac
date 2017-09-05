c4307b49ca9ea84ef8b8fc81947bb0b3095c32a6
lbq <lbq@LEO-MBP.local>
2017-09-05 15:49:54 +0800

    画板

---
+++ DMDrawingBoardForMac/DMDrawingBoardForMac/DMStroke.h
@@ -0,0 +1,15 @@
+//
+//  DMStroke.h
+//  DMDrawingBoardForMac
+//
+//  Created by lbq on 2017/9/5.
+//  Copyright © 2017年 lbq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMStroke : NSObject
+
+@property (nonatomic, copy) NSArray<NSValue *> *points;
+
+@end
---
+++ DMDrawingBoardForMac/DMDrawingBoardForMac/DMStroke.m
@@ -0,0 +1,14 @@
+
+//
+//  DMStroke.m
+//  DMDrawingBoardForMac
+//
+//  Created by lbq on 2017/9/5.
+//  Copyright © 2017年 lbq. All rights reserved.
+//
+
+#import "DMStroke.h"
+
+@implementation DMStroke
+
+@end
